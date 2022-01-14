963e67392a7be0ac10b620c799c33908d54c6ad9
Brian Hwang <brian11hwangb@gmail.com>
2022-01-14 14:50:29 -0800

    4673 finish

---
+++ 4673/4673.cpp
@@ -0,0 +1,36 @@
+
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+#define N 10000
+bool num[N + 1];
+void check(int i);
+
+int main()
+{
+    for (int i = 1; i < N; i++)
+    {
+        check(i);
+    }
+    for (int i = 1; i < N; i++)
+    {
+        bool print = num[i];
+        if (!print)
+            printf("%d\n", i);
+    }
+}
+
+void check(int i)
+{
+    int result = i;
+    while (i != 0)
+    {
+        result += i % 10;
+        i /= 10;
+        // printf("%d %d ", i, result);
+    }
+    printf("\n");
+    if (result <= N)
+        num[result] = true;
+}
