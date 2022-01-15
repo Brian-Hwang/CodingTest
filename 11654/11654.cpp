1a07d2e99af01c86f690da29f51abd56abeaf6ab
Brian Hwang <brian11hwangb@gmail.com>
2022-01-15 01:09:09 -0800

    11654 finish

---
+++ 11654/11654.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    char c;
+    scanf("%c", &c);
+    printf("%d", c);
+    return 0;
+}
