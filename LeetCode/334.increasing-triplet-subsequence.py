735a2e380a9e1269fd403d1521340a54ddbe5f11
Brian-Hwang <brian11hwangb@gmail.com>
2023-08-11 09:24:15 +0900

    update 334. Increasing Triplet Subsequence

--- LeetCode/334.increasing-triplet-subsequence.py
+++ LeetCode/334.increasing-triplet-subsequence.py
@@ -8,29 +8,41 @@
 # @lc code=start
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
-        biggest, middle, i = 0, 1, len(nums) - 1
+        first_min = float("inf")
+        second_min = float("inf")
 
-        while i >= 0:
-            print("BEFORE", biggest, middle, nums[i], i)
-            if i == len(nums) - 1 or nums[i] >= biggest:
-                biggest = nums[i]
-                middle = nums[i - 1]
-                i -= 1
-                while biggest <= middle:
-                    biggest = nums[i]
-                    middle = nums[i - 1]
-                    i -= 1
-                    if i < 1:
-                        return False
-            elif nums[i] >= middle:
-                middle = nums[i]
+        for num in nums:
+            if num <= first_min:
+                first_min = num
+            elif num <= second_min:
+                second_min = num
             else:
                 return True
-            print("AFTER", biggest, middle, i)
-
-            i -= 1
 
         return False
+        # biggest, middle, i = 0, 1, len(nums) - 1
+
+        # while i >= 0:
+        #     print("BEFORE", biggest, middle, nums[i], i)
+        #     if i == len(nums) - 1 or nums[i] >= biggest:
+        #         biggest = nums[i]
+        #         middle = nums[i - 1]
+        #         i -= 1
+        #         while biggest <= middle:
+        #             biggest = nums[i]
+        #             middle = nums[i - 1]
+        #             i -= 1
+        #             if i < 1:
+        #                 return False
+        #     elif nums[i] >= middle:
+        #         middle = nums[i]
+        #     else:
+        #         return True
+        #     print("AFTER", biggest, middle, i)
+
+        #     i -= 1
+
+        # return False
 
 
 # @lc code=end
