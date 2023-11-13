0fd5ea97e59885c9db624f12043f362ee2b3174d
Brian-Hwang <brian11hwangb@gmail.com>
2023-11-13 11:34:39 +0900

    [735]  Asteroid Collision

--- LeetCode/735.asteroid-collision.py
+++ LeetCode/735.asteroid-collision.py
@@ -4,9 +4,32 @@
 # [735] Asteroid Collision
 #
 
+
 # @lc code=start
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-        
-# @lc code=end
+        lefts = []
+        rights = []
+        for asteroid in asteroids:
+            # rights
+            if asteroid > 0:
+                rights.append(asteroid)
 
+            # lefts
+            else:
+                flag = 0
+                while len(rights) != 0:
+                    if abs(asteroid) > rights[-1]:
+                        rights.pop()
+                    else:
+                        if abs(asteroid) == rights[-1]:
+                            rights.pop()
+                        flag = 1
+                        break
+                if len(rights) == 0 and flag == 0:
+                    lefts.append(asteroid)
+
+        return lefts + rights
+
+
+# @lc code=end
