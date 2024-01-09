a2d04377d83d1ebd546673f7e035b26916531de7
Brian-Hwang <brian11hwangb@gmail.com>
2024-01-09 22:24:06 +0900

    [746] Min Cost Climbing Stairs

---
+++ LeetCode/746.min-cost-climbing-stairs.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=746 lang=python3
+#
+# [746] Min Cost Climbing Stairs
+#
+
+
+# @lc code=start
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        for i in range(2, len(cost)):
+            cost[i] += min(cost[i - 1], cost[i - 2])
+
+        return min(cost[-1], cost[-2])
+
+
+# @lc code=end
