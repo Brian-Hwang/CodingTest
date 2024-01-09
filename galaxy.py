af4c7e9a6949f0f6541fdb508a80c2c460b615b5
Brian-Hwang <brian11hwangb@gmail.com>
2024-01-09 10:24:56 +0900

    update galaxy

--- galaxy.py
+++ galaxy.py
@@ -1,4 +1,12 @@
-def solve_case(triples):
+def get_test_cases():
+    with open("test_cases2.txt", "r") as file:
+        test_cases = file.read().splitlines()
+        test_case_num = test_cases[0]
+
+    return test_case_num, test_cases
+
+
+def min_sacrifice(triples):
     n = len(triples)
     INF = float("inf")
     dp = [[[INF for _ in range(2)] for _ in range(2)] for _ in range(n + 1)]
@@ -8,22 +16,11 @@ def solve_case(triples):
         a, b, c = triples[i - 1]
         for j in range(2):
             for k in range(2):
-                # Choose 'a'
                 dp[i][1][k] = min(dp[i][1][k], dp[i - 1][j][k] + b + c)
-                # Choose 'b'
                 dp[i][j][1] = min(dp[i][j][1], dp[i - 1][j][k] + a + c)
-                # Choose 'c'
                 dp[i][j][k] = min(dp[i][j][k], dp[i - 1][j][k] + a + b)
 
     return min(dp[n][1][1], dp[n][1][0] + triples[-1][1], dp[n][0][1] + triples[-1][0])
-
-
-def get_test_cases():
-    with open("test_cases2.txt", "r") as file:
-        test_cases = file.read().splitlines()
-        test_case_num = test_cases[0]
-
-    return test_case_num, test_cases
 
 
 if __name__ == "__main__":
@@ -44,5 +41,5 @@ if __name__ == "__main__":
             test_case_line += 1
             triples.append((him, ji, minchup))
 
-        result = solve_case(triples)
-        print(f"#{i} {result}")
+        sacrifice = min_sacrifice(triples)
+        print(f"#{i} {sacrifice}")
