e7b56767591d8b6b3cb08c8b930f6981426ba5f1
Brian-Hwang <brian11hwangb@gmail.com>
2024-01-17 20:04:34 +0900

    [136] Single Number

---
+++ LeetCode/136.single-number.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=136 lang=python3
+#
+# [136] Single Number
+#
+
+
+# @lc code=start
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        ans = 0
+        for num in nums:
+            ans ^= num
+        return ans
+
+
+# @lc code=end
